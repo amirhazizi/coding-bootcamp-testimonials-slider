1c0becf29202addb2e0598e03d30d6bd080dc7ef
amir <amirazizihosein@gmail.com>
2023-03-06 11:54:47 +0330

    slide user image animation

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ import persons from "./data.js"
 // variables
 const nextBtn = document.querySelector("#next-btn")
 const prevBtn = document.querySelector("#prev-btn")
-const userImage = document.querySelector(".avatar")
 const userText = document.querySelector(".user-desc")
 const userName = document.querySelector(".user-name")
 const userProfes = document.querySelector(".user-profes")
+const userImages = document.querySelector(".avatar-container")
 // index
 let index = 0
 
@@ -15,6 +15,8 @@ let index = 0
 nextBtn.addEventListener("click", () => {
   index++
   indexChecker()
+  showSlider()
+
   showUser(index)
 })
 
@@ -22,25 +24,38 @@ nextBtn.addEventListener("click", () => {
 prevBtn.addEventListener("click", () => {
   index--
   indexChecker()
+  showSlider("prev")
   showUser(index)
 })
 
 // DOMContentLoaded event
 window.addEventListener("DOMContentLoaded", () => {
   showUser(index)
+  const personImages = persons
+    .map((person, index) => {
+      const { image, name } = person
+      let position = "next"
+      if (index === 0) position = "active"
+      if (index === persons.length - 1) position = "last"
+      return `<img
+              class="avatar ${position} rounded-md mx-auto drop-shadow-2xl absolute bottom-0 left-0"
+              src='${image}'
+              alt='${name}'
+            />`
+    })
+    .join("")
+  userImages.innerHTML += personImages
 })
 
 // user switcher function
 function showUser(index) {
   let personNumber = index
-  console.log("personNum", personNumber)
   const {
     image: imageSrc,
     name,
     professional: prof,
     text,
   } = persons[personNumber]
-  userImage.setAttribute("src", imageSrc)
   userName.innerHTML = name
   userProfes.innerHTML = prof
   userText.innerHTML = text
@@ -50,4 +65,29 @@ function showUser(index) {
 function indexChecker() {
   if (index >= persons.length) index = 0
   if (index < 0) index = persons.length - 1
+}
+
+// slider images func
+function showSlider(type) {
+  const active = document.querySelector(".active")
+  const last = document.querySelector(".last")
+  let next = active.nextElementSibling || userImages.firstElementChild
+  if (next.classList.contains("opacity-0")) next = next.nextElementSibling
+  active.classList.remove("active")
+  last.classList.remove("last")
+  next.classList.remove("next")
+  if (type === "prev") {
+    active.classList.add("last")
+    last.classList.add("next")
+    next = active.previousElementSibling || userImages.lastElementChild
+    if (next.classList.contains("opacity-0")) next = userImages.lastElementChild
+    next.classList.add("active")
+    next.classList.remove("next")
+    return
+  }
+
+  //   active.classList.remove("next")
+  last.classList.remove("next")
+  next.classList.add("active")
+  active.classList.add("last")
 }
--- js/data.js
+++ js/data.js
@@ -15,5 +15,22 @@ const persons = [
     course. The depth the instructors go into is incredible. I now feel so
     confident about starting up as a professional developer. ”`,
   },
+  // for currect slide animation
+  {
+    image: "./images/image-tanya.jpg",
+    name: "Tanya Sinclair",
+    professional: "UX Engineer",
+    text: `“ I’ve been interested in coding for a while but never taken the jump,
+          until now. I couldn’t recommend this course enough. I’m now in the job
+          of my dreams and so excited about the future. ”`,
+  },
+  {
+    image: "./images/image-john.jpg",
+    name: "John Tarkpor",
+    professional: " Junior Front-end Developer",
+    text: `“ If you want to lay the best foundation possible I’d recommend taking this
+    course. The depth the instructors go into is incredible. I now feel so
+    confident about starting up as a professional developer. ”`,
+  },
 ]
 export default persons
